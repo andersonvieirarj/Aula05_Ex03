b65c714617aebe7c0884f589d4bf5a6ac244b6fa
andersonvieirarj <andersonvieirarj@gmail.com>
2023-04-03 17:40:31 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int quantAlunos;
+        double nota;
+        double media=0;
+        double somatorio=0;
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Informe a quantidade de alunos:");
+        quantAlunos = leitura.nextInt();
+
+        for (int i=1; i<=quantAlunos; i++) {
+            System.out.println("Informe a média do " + i + ".° aluno:");
+            nota = leitura.nextDouble();
+            somatorio = somatorio + nota;
+        }
+        media = somatorio/quantAlunos;
+        System.out.println("Média geral dos alunos:" + media);
+
+    }
+}
